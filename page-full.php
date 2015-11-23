698133c24d2f91cac5e8de10019c0da9bd990827
mikeybell <michaelrobertbell@gmail.com>
2015-11-23 12:06:25 -0500

    Beer work

--- page-full.php
+++ page-full.php
@@ -21,14 +21,43 @@ get_header();  ?>
   )); ?>
 </nav>
 
+<!-- BEER -->
+
 <section class="beer" id="beer">
 	<div class="header-box">
 		<h2>Beer</h2>
 	</div> <!-- /.header-box -->
 	
-	<div class="container">
+	<div class="container pad beer-box">
 		
-	
+		<div class="six-col">
+			<!-- START QUERY -->
+			<?php $beerQuery = new WP_Query(
+			  array(
+			    'post_type'=>'beer',
+			    'order'=> 'ASC'
+			    )
+			); ?>
+			
+			<!-- LOOP -->
+			<?php if ($beerQuery->have_posts()): ?>
+			  <?php while ($beerQuery->have_posts()): $beerQuery->the_post(); ?>
+			   <!-- stuff goes here -->
+			   <h3><?php the_title(); ?></h3>
+				<ul class="beer-list">
+					<?php while(has_sub_field('beer_names')): ?>
+					<li><?php the_sub_field('beer_name'); ?> - <?php the_sub_field('percentage'); ?></li>
+					<?php endwhile; ?>
+				</ul>
+
+			  <?php endwhile; ?>    
+			  <?php wp_reset_postdata(); ?>
+			<?php endif; ?>
+			<!-- END QUERY -->
+		</div> <!-- /.six-col -->
+
+
+
 	</div> <!-- /.container -->
 </section> <!-- /.beer -->
 
